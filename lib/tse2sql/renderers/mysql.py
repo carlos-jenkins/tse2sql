7a15f792eab52ad53dccbbaa60ad20e401cd1eeb
Carlos Miguel Jenkins Perez <carlos@jenkins.co.cr>
2016-02-12 20:09:31 -0600

    chg: dev: Added characters, checking and batch improvements to the INSERT INTO sql.

--- lib/tse2sql/renderers/mysql.py
+++ lib/tse2sql/renderers/mysql.py
@@ -120,11 +120,6 @@ CREATE TABLE IF NOT EXISTS `tsesql`.`voter` (
 ENGINE = InnoDB;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
 """  # noqa
 
 SECTION_HEADER = """\
@@ -133,26 +128,34 @@ SECTION_HEADER = """\
 -- -----------------------------------------------------
 """
 
+FOOTER = """\
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+"""
+
 
 def write_provinces(fd, provinces):
     """
     Write provinces INSERT INTO statement.
     """
     fd.write(SECTION_HEADER.format(name='province'))
+    fd.write('SET AUTOCOMMIT=0;\nINSERT INTO `province` VALUES\n')
 
     with tqdm(
             total=len(provinces), unit='e', leave=True,
             desc='INSERT INTO province') as pbar:
 
         for province_code, name in provinces.items():
-            fd.write('INSERT INTO province VALUES (')
+            fd.write('(')
             fd.write(str(province_code))
             fd.write(', \'')
             fd.write(name)
-            fd.write('\');\n')
+            fd.write('\'),\n')
             pbar.update(1)
 
-    fd.write('\n\n')
+    fd.seek(fd.tell() - 2)
+    fd.write(';\nCOMMIT;\n\n\n')
 
 
 def write_cantons(fd, cantons):
@@ -160,23 +163,25 @@ def write_cantons(fd, cantons):
     Write cantons INSERT INTO statement.
     """
     fd.write(SECTION_HEADER.format(name='canton'))
+    fd.write('SET AUTOCOMMIT=0;\nINSERT INTO `canton` VALUES\n')
 
     with tqdm(
             total=len(cantons), unit='e', leave=True,
             desc='INSERT INTO canton') as pbar:
 
         for (province_code, canton_code), name in cantons.items():
-            fd.write('INSERT INTO canton VALUES (')
+            fd.write('(')
             fd.write(str(province_code))
             fd.write('{:02d}'.format(canton_code))
             fd.write(', \'')
             fd.write(name)
             fd.write('\', ')
             fd.write(str(province_code))
-            fd.write(');\n')
+            fd.write('),\n')
             pbar.update(1)
 
-    fd.write('\n\n')
+    fd.seek(fd.tell() - 2)
+    fd.write(';\nCOMMIT;\n\n\n')
 
 
 def write_districts(fd, districts):
@@ -184,14 +189,23 @@ def write_districts(fd, districts):
     Write districts INSERT INTO statement.
     """
     fd.write(SECTION_HEADER.format(name='district'))
+    fd.write('SET AUTOCOMMIT=0;\nINSERT INTO `district` VALUES\n')
 
     with tqdm(
             total=len(districts), unit='e', leave=True,
             desc='INSERT INTO district') as pbar:
 
-        for (province_code, canton_code, district_code), name \
-                in districts.items():
-            fd.write('INSERT INTO district VALUES (')
+        for num, ((province_code, canton_code, district_code), name) \
+                in enumerate(districts.items()):
+
+            if num % 1000 == 0 and num > 0:
+                fd.seek(fd.tell() - 2)
+                fd.write(
+                    ';\nCOMMIT;\n\n\nSET AUTOCOMMIT=0;\n'
+                    'INSERT INTO `district` VALUES\n'
+                )
+
+            fd.write('(')
             fd.write(str(province_code))
             fd.write('{:02d}'.format(canton_code))
             fd.write('{:03d}'.format(district_code))
@@ -200,10 +214,11 @@ def write_districts(fd, districts):
             fd.write('\', ')
             fd.write(str(province_code))
             fd.write('{:02d}'.format(canton_code))
-            fd.write(');\n')
+            fd.write('),\n')
             pbar.update(1)
 
-    fd.write('\n\n')
+    fd.seek(fd.tell() - 2)
+    fd.write(';\nCOMMIT;\n\n\n')
 
 
 def write_voters(fd, voters):
@@ -211,13 +226,22 @@ def write_voters(fd, voters):
     Write voters INSERT INTO statement.
     """
     fd.write(SECTION_HEADER.format(name='voter'))
+    fd.write('SET AUTOCOMMIT=0;\nINSERT INTO `voter` VALUES\n')
 
     with tqdm(
             total=voters.total_voters, unit='v', leave=True,
             unit_scale=True, desc='INSERT INTO voter') as pbar:
 
-        for voter in voters:
-            fd.write('INSERT INTO voter VALUES (')
+        for num, voter in enumerate(voters):
+
+            if num % 1000 == 0 and num > 0:
+                fd.seek(fd.tell() - 2)
+                fd.write(
+                    ';\nCOMMIT;\n\n\nSET AUTOCOMMIT=0;\n'
+                    'INSERT INTO `voter` VALUES\n'
+                )
+
+            fd.write('(')
             fd.write(str(voter['id'])),
             fd.write(', ')
             fd.write(str(voter['sex'])),
@@ -233,10 +257,11 @@ def write_voters(fd, voters):
             fd.write(voter['family_name_2'])
             fd.write('\', ')
             fd.write(str(voter['district']))
-            fd.write(');\n')
+            fd.write('),\n')
             pbar.update(1)
 
-    fd.write('\n\n')
+    fd.seek(fd.tell() - 2)
+    fd.write(';\nCOMMIT;\n\n\n')
 
 
 def write_mysql(fd, payload):
@@ -252,6 +277,7 @@ def write_mysql(fd, payload):
     write_cantons(fd, payload['cantons'])
     write_districts(fd, payload['districts'])
     write_voters(fd, payload['voters'])
+    fd.write(FOOTER)
 
 
 __all__ = ['write_mysql']
