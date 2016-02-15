e3470453b28ca16d04dd0ed2e91803a241c2860b
Carolina Aguilar <caguilar@backcountry.com>
2016-02-15 13:25:26 -0600

    new: dev: Adding voter info endpoint.

--- apis/node-api/routes/index.js
+++ apis/node-api/routes/index.js
@@ -1,3 +1,11 @@
-module.exports = function(app) {
+var voterService = require('../lib/voter-info-service');
 
+module.exports = function(app) {
+    app.get('/voter-info/:voterId', function(req, res) {
+        voterService.getInfo(req.params.voterId, function(err, result) {
+            if(!err) {
+                res.send(result);
+            }
+        });
+    });
 };
