bcfa9d7ad71854a3017d9bed1970ecd6c4537f6b
Carolina Aguilar <caguilar@backcountry.com>
2016-02-15 19:45:52 -0600

    fix: dev: Breaking query string into multiple lines

--- apis/node-api/lib/voter-info-service/index.js
+++ apis/node-api/lib/voter-info-service/index.js
@@ -2,12 +2,16 @@ var _ = require('underscore');
 var db = require('../../db');
 
 var voter = {};
-var queries = {
-    voterInfo:  'SELECT id_voter, name, family_name_1, family_name_2, sex, id_expiration, name_province, name_canton, name_district, site FROM voter JOIN district ON voter.district_id_district = district.id_district JOIN canton ON district.canton_id_canton = canton.id_canton JOIN province ON canton.province_id_province = province.id_province WHERE voter.id_voter = ?;'
-};
+
+var voterByIdQuery = 'SELECT id_voter, name, family_name_1, family_name_2, sex, id_expiration, ' +
+                'name_province, name_canton, name_district, site FROM voter ' +
+                'JOIN district ON voter.district_id_district = district.id_district ' +
+                'JOIN canton ON district.canton_id_canton = canton.id_canton '+
+                'JOIN province ON canton.province_id_province = province.id_province ' +
+                'WHERE voter.id_voter = ?;';
 
 voter.getInfo = function(voterId, callback) {
-    db.query(queries.voterInfo, [voterId], function(err, rows, fields) {
+    db.query(voterByIdQuery, [voterId], function(err, rows, fields) {
         callback(null, _.first(rows));
     });
 };
