8012867030f2931203a9609425d1ce3de1405ffc
Carolina Aguilar <caguilar@backcountry.com>
2016-02-15 23:01:09 -0600

    fix: dev: Improving query to search voters by name to get more accurate results

--- apis/node-api/lib/voter-info-service/index.js
+++ apis/node-api/lib/voter-info-service/index.js
@@ -1,19 +1,18 @@
 var _ = require('underscore');
 var db = require('../../db');
+var dbMinTokenSize = 3;
 
 var voter = {};
 
-var baseQuery = 'SELECT id_voter, name, family_name_1, family_name_2, sex, id_expiration, ' +
+var voterByIdQuery = 'SELECT id_voter, name, family_name_1, family_name_2, sex, id_expiration, ' +
                 'name_province, name_canton, name_district, site FROM voter ' +
                 'JOIN district ON voter.district_id_district = district.id_district ' +
                 'JOIN canton ON district.canton_id_canton = canton.id_canton ' +
-                'JOIN province ON canton.province_id_province = province.id_province ';
+                'JOIN province ON canton.province_id_province = province.id_province ' +
+                'WHERE voter.id_voter = ?;';
 
-var voterByIdQuery = baseQuery + 'WHERE voter.id_voter = ?;';
-
-var voterByNameQuery = baseQuery + 'WHERE voter.name LIKE ? ' +
-                'AND voter.family_name_1 LIKE ? ' +
-                'AND voter.family_name_2 LIKE ? ;'
+var voterByNameQuery = 'SELECT * FROM voter WHERE MATCH(name, family_name_1, family_name_2) ' +
+                    ' AGAINST (? IN BOOLEAN MODE);'
 
 voter.getVoterById = function(params, type, callback) {
     db.query(voterByIdQuery, params, function(err, rows, fields) {
@@ -22,11 +21,23 @@ voter.getVoterById = function(params, type, callback) {
 };
 
 voter.getVoterByName = function(voterName, type, callback) {
-    var params = _.map(voterName.split(' '), function(token) {
-        return '%' + token + '%';
-    });
+    voterName = _.map(voterName.split(' '), function(token) {
+        /*
+            To implement the full search, MySQL uses Boolean logic, in which
+            + stands for AND
+            - stands for NOT
+            [no operator] implies OR
 
-    db.query(voterByNameQuery, params, function(err, rows, fields) {
+            The minimum default token size in InnoDB is 3 characters and the
+            indexing engine ignores words shorter than this minimum size, then in
+            our case when the length of the token is minor than 3 the tokenOperator
+            is not added, otherwise we add the + operator
+        */
+        var tokenOperator = (token.length < dbMinTokenSize) ? '' : '+';
+        return tokenOperator + token;
+    }).join(' ');
+
+    db.query(voterByNameQuery, ['\'' + voterName + '\''], function(err, rows, fields) {
         callback(null, rows);
     });
 };
