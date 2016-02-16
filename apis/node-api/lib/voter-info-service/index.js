0044ec7917591444562fb12d2810bd341c1edbe8
Carolina Aguilar <caroagse@gmail.com>
2016-02-16 13:53:52 -0600

    fix: usr: Fixing query and unnecessary params in /voter/info-by-id endpoint

--- apis/node-api/lib/voter-info-service/index.js
+++ apis/node-api/lib/voter-info-service/index.js
@@ -7,7 +7,7 @@ var voter = {};
 var voterByIdQuery =
     'SELECT id_voter, name, family_name_1, family_name_2, sex, ' +
     'id_expiration, name_province, name_canton, name_district, site ' +
-    'FROM voter' +
+    'FROM voter ' +
     'JOIN district ON voter.district_id_district = district.id_district ' +
     'JOIN canton ON district.canton_id_canton = canton.id_canton ' +
     'JOIN province ON canton.province_id_province = province.id_province ' +
@@ -18,13 +18,14 @@ var voterByNameQuery =
     'WHERE MATCH(name, family_name_1, family_name_2) ' +
     'AGAINST (? IN BOOLEAN MODE) LIMIT 30;';
 
-voter.getVoterById = function(params, type, callback) {
-    db.query(voterByIdQuery, params, function(err, rows, fields) {
-        callback(null, _.first(rows));
+voter.getVoterById = function(voterId, callback) {
+    db.query(voterByIdQuery, [voterId],
+        function(err, rows, fields) {
+            callback(null, _.first(rows));
     });
 };
 
-voter.getVoterByName = function(voterName, type, callback) {
+voter.getVoterByName = function(voterName, callback) {
     voterName = _.map(voterName.split(' '), function(token) {
         /*
             To implement the full search, MySQL uses Boolean logic, in which:
--- apis/node-api/routes/index.js
+++ apis/node-api/routes/index.js
@@ -2,7 +2,7 @@ var voterService = require('../lib/voter-info-service');
 
 module.exports = function(app) {
     app.get('/voter/info-by-id/:voterId', function(req, res) {
-        voterService.getVoterById(req.params.voterId, 'id', function(err, result) {
+        voterService.getVoterById(req.params.voterId, function(err, result) {
             if(!err) {
                 res.send(result);
             }
@@ -10,7 +10,7 @@ module.exports = function(app) {
     });
 
     app.get('/voter/info-by-name/:voterName', function(req, res) {
-        voterService.getVoterByName(req.params.voterName, 'name', function(err, result) {
+        voterService.getVoterByName(req.params.voterName, function(err, result) {
             if(!err) {
                 res.send(result);
             }
