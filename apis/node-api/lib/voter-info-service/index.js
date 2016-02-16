bc45d00b20c0ebf9cb22206a736043470dbec00e
Carolina Aguilar <caguilar@backcountry.com>
2016-02-15 21:10:46 -0600

    new: dev: Adding endpoint to node api to search voter by name

--- apis/node-api/lib/voter-info-service/index.js
+++ apis/node-api/lib/voter-info-service/index.js
@@ -3,16 +3,31 @@ var db = require('../../db');
 
 var voter = {};
 
-var voterByIdQuery = 'SELECT id_voter, name, family_name_1, family_name_2, sex, id_expiration, ' +
+var baseQuery = 'SELECT id_voter, name, family_name_1, family_name_2, sex, id_expiration, ' +
                 'name_province, name_canton, name_district, site FROM voter ' +
                 'JOIN district ON voter.district_id_district = district.id_district ' +
-                'JOIN canton ON district.canton_id_canton = canton.id_canton '+
-                'JOIN province ON canton.province_id_province = province.id_province ' +
-                'WHERE voter.id_voter = ?;';
+                'JOIN canton ON district.canton_id_canton = canton.id_canton ' +
+                'JOIN province ON canton.province_id_province = province.id_province ';
 
-voter.getInfo = function(voterId, callback) {
-    db.query(voterByIdQuery, [voterId], function(err, rows, fields) {
+var voterByIdQuery = baseQuery + 'WHERE voter.id_voter = ?;';
+
+var voterByNameQuery = baseQuery + 'WHERE voter.name LIKE ? ' +
+                'AND voter.family_name_1 LIKE ? ' +
+                'AND voter.family_name_2 LIKE ? ;'
+
+voter.getVoterById = function(params, type, callback) {
+    db.query(voterByIdQuery, params, function(err, rows, fields) {
         callback(null, _.first(rows));
+    });
+};
+
+voter.getVoterByName = function(voterName, type, callback) {
+    var params = _.map(voterName.split(' '), function(token) {
+        return '%' + token + '%';
+    });
+
+    db.query(voterByNameQuery, params, function(err, rows, fields) {
+        callback(null, rows);
     });
 };
 
--- apis/node-api/routes/index.js
+++ apis/node-api/routes/index.js
@@ -1,8 +1,16 @@
 var voterService = require('../lib/voter-info-service');
 
 module.exports = function(app) {
-    app.get('/voter-info/:voterId', function(req, res) {
-        voterService.getInfo(req.params.voterId, function(err, result) {
+    app.get('/voter/info-by-id/:voterId', function(req, res) {
+        voterService.getVoterById(req.params.voterId, 'id', function(err, result) {
+            if(!err) {
+                res.send(result);
+            }
+        });
+    });
+
+    app.get('/voter/info-by-name/:voterName', function(req, res) {
+        voterService.getVoterByName(req.params.voterName, 'name', function(err, result) {
             if(!err) {
                 res.send(result);
             }
