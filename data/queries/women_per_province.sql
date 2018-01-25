95d5254be4d88006d705d0f271a4be9515bb14ae
Carolina Aguilar <carolina.aguilar@kuralabs.io>
2018-01-25 02:19:46 -0600

    fix: dev: Added missing ORDER BY statement to data query.

--- data/queries/women_per_province.sql
+++ data/queries/women_per_province.sql
@@ -3,19 +3,20 @@ FROM voter
 JOIN district ON voter.district_id_district = district.id_district
 JOIN canton ON district.canton_id_canton = canton.id_canton
 JOIN province ON canton.province_id_province = province.id_province
-AND voter.sex = 2 GROUP BY province.name_province;
+AND voter.sex = 2 GROUP BY province.name_province
+ORDER BY Women DESC;
 
 /*
 +--------+---------------+
 | Women  | name_province |
 +--------+---------------+
+| 571490 | San Jose      |
 | 312920 | Alajuela      |
 | 195205 | Cartago       |
-|  15336 | Consulado     |
-| 119283 | Guanacaste    |
 | 169184 | Heredia       |
-| 132177 | Limon         |
 | 151629 | Puntarenas    |
-| 571490 | San Jose      |
+| 132177 | Limon         |
+| 119283 | Guanacaste    |
+|  15336 | Consulado     |
 +--------+---------------+
 */
